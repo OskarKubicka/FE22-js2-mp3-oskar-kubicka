0a8a5545f381c32e39fd7372131bdafb1da482d1
Oskar Ku <oskar.kubicka@gritacademy.se>
2023-03-09 17:01:25 +0100

    stor bokstav

--- script/produkter.js
+++ script/produkter.js
@@ -57,7 +57,10 @@ async function getProducts() {
         div.className = 'div-prod'
         const h1 = document.createElement('h1');
         div.append(h1);
-        h1.innerText = namn;
+        let string = namn;
+        h1.innerHTML = string.charAt(0).toUpperCase() +
+        string.slice(1);
+        
         const prisP = document.createElement('p');
         div.append(prisP);
         prisP.innerText = pris + ' kr';
